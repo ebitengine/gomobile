Mu-L/mobile

cmd/gomobile: detect missing golang.org/x/mobile dep before invoking gobind
gobind's generated code depends on packages under golang.org/x/mobile/bind,
and gobind resolves them through the calling module via packages.Load. When
the dependency is missing from the user's module — most commonly after
go mod tidy removes it because no non-generated code imports it — gobind
fails with an opaque message repeated once per sub-target:

    unable to import bind: no Go package in golang.org/x/mobile/bind
    unable to import bind: no Go package in golang.org/x/mobile/bind
    unable to import bind/objc: no Go package in golang.org/x/mobile/bind/objc

Detect the missing module dependency in cmd/gomobile before invoking gobind
and emit a single actionable message that suggests adding a tool directive
to go.mod via 'go get -tool golang.org/x/mobile/cmd/gobind'.

Fixes golang/go#77183.

Change-Id: I365c99a1f37bec1f42b6cb858babd33d41317a8a
Reviewed-on: https://go-review.googlesource.com/c/mobile/+/779840
Reviewed-by: Dmitri Shuralyov <dmitshur@google.com>
Reviewed-by: Michael Pratt <mpratt@google.com>
Reviewed-by: Hajime Hoshi <hajimehoshi@gmail.com>
LUCI-TryBot-Result: golang-scoped@luci-project-accounts.iam.gserviceaccount.com <golang-scoped@luci-project-accounts.iam.gserviceaccount.com>
Auto-Submit: Hajime Hoshi <hajimehoshi@gmail.com>

cmd/gomobile/bind.go
@@ -84,6 +84,19 @@ func runBind(cmd *command) error {
 		return fmt.Errorf(`invalid -target=%q: %v`, buildTarget, err)
 	}
 
+	if !mobileModuleAvailable() {
+		fmt.Fprintln(os.Stderr, `gomobile bind requires golang.org/x/mobile in the current module, but it is not in the module dependency graph.
+
+Add it with:
+
+	go get -tool golang.org/x/mobile/cmd/gobind
+
+This records a tool directive in go.mod so subsequent go mod tidy runs keep
+the dependency. See https://go.dev/doc/modules/managing-dependencies#tools
+for details and https://go.dev/issue/77183 for background.`)
+		return errors.New("missing golang.org/x/mobile dependency")
+	}
+
 	if isAndroidPlatform(targets[0].platform) {
 		if bindPrefix != "" {
 			return fmt.Errorf("-prefix is supported only for Apple targets")
@@ -353,4 +366,16 @@ func areGoModulesUsed() (bool, error) {
 		areGoModulesUsedResult.used = outstr != ""
 	})
 	return areGoModulesUsedResult.used, areGoModulesUsedResult.err
+}
+
+// mobileModuleAvailable reports whether golang.org/x/mobile/bind is
+// resolvable through the current module. In GOPATH mode or when the module
+// probe fails, it returns true and lets gobind surface any error itself.
+func mobileModuleAvailable() bool {
+	modulesUsed, err := areGoModulesUsed()
+	if err != nil || !modulesUsed {
+		return true
+	}
+	pkgs, err := packages.Load(&packages.Config{Mode: packages.NeedName}, "golang.org/x/mobile/bind")
+	return err == nil && len(pkgs) == 1 && pkgs[0].Name != "" && len(pkgs[0].Errors) == 0
 }

cmd/gomobile/bind_test.go
@@ -402,3 +402,65 @@ func TestBindWithGoModules(t *testing.T) {
 		})
 	}
 }
+
+func TestBindMissingMobileModule(t *testing.T) {
+	if runtime.GOOS == "android" || runtime.GOOS == "ios" {
+		t.Skipf("gomobile and gobind are not available on %s", runtime.GOOS)
+	}
+
+	dir := t.TempDir()
+
+	if out, err := exec.Command("go", "build", "-o="+dir, "golang.org/x/mobile/cmd/gobind").CombinedOutput(); err != nil {
+		t.Fatalf("building gobind: %v: %s", err, string(out))
+	}
+	if out, err := exec.Command("go", "build", "-o="+dir, "golang.org/x/mobile/cmd/gomobile").CombinedOutput(); err != nil {
+		t.Fatalf("building gomobile: %v: %s", err, string(out))
+	}
+	path := dir
+	if p := os.Getenv("PATH"); p != "" {
+		path += string(filepath.ListSeparator) + p
+	}
+
+	projDir := filepath.Join(dir, "noxmobile")
+	if err := os.Mkdir(projDir, 0755); err != nil {
+		t.Fatal(err)
+	}
+	goVer := strings.TrimPrefix(runtime.Version(), "go")
+	if parts := strings.SplitN(goVer, ".", 3); len(parts) >= 2 {
+		goVer = parts[0] + "." + parts[1]
+	}
+	goMod := `module example.com/noxmobile
+
+go ` + goVer + "\n"
+	if err := os.WriteFile(filepath.Join(projDir, "go.mod"), []byte(goMod), 0644); err != nil {
+		t.Fatal(err)
+	}
+	const helloGo = `package noxmobile
+
+func Hello() string { return "hi" }
+`
+	if err := os.WriteFile(filepath.Join(projDir, "hello.go"), []byte(helloGo), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	cmd := exec.Command(filepath.Join(dir, "gomobile"), "bind", "-target=ios", ".")
+	cmd.Env = append(os.Environ(), "PATH="+path)
+	cmd.Dir = projDir
+	out, err := cmd.CombinedOutput()
+	if err == nil {
+		t.Fatalf("gomobile bind unexpectedly succeeded\noutput:\n%s", out)
+	}
+	got := string(out)
+	for _, want := range []string{
+		"gomobile bind requires golang.org/x/mobile",
+		"go get -tool golang.org/x/mobile/cmd/gobind",
+		"go.dev/issue/77183",
+	} {
+		if !strings.Contains(got, want) {
+			t.Errorf("error message does not contain %q\noutput:\n%s", want, got)
+		}
+	}
+	if strings.Contains(got, "no Go package in golang.org/x/mobile/bind") {
+		t.Errorf("error message still contains the opaque gobind failure\noutput:\n%s", got)
+	}
+}

ebitengine/gomobile

cmd/gomobile/bind.go
@@ -84,6 +84,19 @@ func runBind(cmd *command) error {
 		return fmt.Errorf(`invalid -target=%q: %v`, buildTarget, err)
 	}
 
+	if !mobileModuleAvailable() {
+		fmt.Fprintln(os.Stderr, `gomobile bind requires github.com/ebitengine/gomobile in the current module, but it is not in the module dependency graph.
+
+Add it with:
+
+	go get -tool github.com/ebitengine/gomobile/cmd/gobind
+
+This records a tool directive in go.mod so subsequent go mod tidy runs keep
+the dependency. See https://go.dev/doc/modules/managing-dependencies#tools
+for details and https://go.dev/issue/77183 for background.`)
+		return errors.New("missing github.com/ebitengine/gomobile dependency")
+	}
+
 	if isAndroidPlatform(targets[0].platform) {
 		if bindPrefix != "" {
 			return fmt.Errorf("-prefix is supported only for Apple targets")
@@ -353,4 +366,16 @@ func areGoModulesUsed() (bool, error) {
 		areGoModulesUsedResult.used = outstr != ""
 	})
 	return areGoModulesUsedResult.used, areGoModulesUsedResult.err
+}
+
+// mobileModuleAvailable reports whether github.com/ebitengine/gomobile/bind is
+// resolvable through the current module. In GOPATH mode or when the module
+// probe fails, it returns true and lets gobind surface any error itself.
+func mobileModuleAvailable() bool {
+	modulesUsed, err := areGoModulesUsed()
+	if err != nil || !modulesUsed {
+		return true
+	}
+	pkgs, err := packages.Load(&packages.Config{Mode: packages.NeedName}, "github.com/ebitengine/gomobile/bind")
+	return err == nil && len(pkgs) == 1 && pkgs[0].Name != "" && len(pkgs[0].Errors) == 0
 }

cmd/gomobile/bind_test.go
@@ -402,3 +402,65 @@ func TestBindWithGoModules(t *testing.T) {
 		})
 	}
 }
+
+func TestBindMissingMobileModule(t *testing.T) {
+	if runtime.GOOS == "android" || runtime.GOOS == "ios" {
+		t.Skipf("gomobile and gobind are not available on %s", runtime.GOOS)
+	}
+
+	dir := t.TempDir()
+
+	if out, err := exec.Command("go", "build", "-o="+dir, "github.com/ebitengine/gomobile/cmd/gobind").CombinedOutput(); err != nil {
+		t.Fatalf("building gobind: %v: %s", err, string(out))
+	}
+	if out, err := exec.Command("go", "build", "-o="+dir, "github.com/ebitengine/gomobile/cmd/gomobile").CombinedOutput(); err != nil {
+		t.Fatalf("building gomobile: %v: %s", err, string(out))
+	}
+	path := dir
+	if p := os.Getenv("PATH"); p != "" {
+		path += string(filepath.ListSeparator) + p
+	}
+
+	projDir := filepath.Join(dir, "noxmobile")
+	if err := os.Mkdir(projDir, 0755); err != nil {
+		t.Fatal(err)
+	}
+	goVer := strings.TrimPrefix(runtime.Version(), "go")
+	if parts := strings.SplitN(goVer, ".", 3); len(parts) >= 2 {
+		goVer = parts[0] + "." + parts[1]
+	}
+	goMod := `module example.com/noxmobile
+
+go ` + goVer + "\n"
+	if err := os.WriteFile(filepath.Join(projDir, "go.mod"), []byte(goMod), 0644); err != nil {
+		t.Fatal(err)
+	}
+	const helloGo = `package noxmobile
+
+func Hello() string { return "hi" }
+`
+	if err := os.WriteFile(filepath.Join(projDir, "hello.go"), []byte(helloGo), 0644); err != nil {
+		t.Fatal(err)
+	}
+
+	cmd := exec.Command(filepath.Join(dir, "gomobile"), "bind", "-target=ios", ".")
+	cmd.Env = append(os.Environ(), "PATH="+path)
+	cmd.Dir = projDir
+	out, err := cmd.CombinedOutput()
+	if err == nil {
+		t.Fatalf("gomobile bind unexpectedly succeeded\noutput:\n%s", out)
+	}
+	got := string(out)
+	for _, want := range []string{
+		"gomobile bind requires github.com/ebitengine/gomobile",
+		"go get -tool github.com/ebitengine/gomobile/cmd/gobind",
+		"go.dev/issue/77183",
+	} {
+		if !strings.Contains(got, want) {
+			t.Errorf("error message does not contain %q\noutput:\n%s", want, got)
+		}
+	}
+	if strings.Contains(got, "no Go package in github.com/ebitengine/gomobile/bind") {
+		t.Errorf("error message still contains the opaque gobind failure\noutput:\n%s", got)
+	}
+}